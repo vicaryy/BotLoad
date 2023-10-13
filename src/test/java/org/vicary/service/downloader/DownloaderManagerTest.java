5676ad6759e7ec4d595d793f32f16db3fa1baf3b
vicary <vicarycholewa@gmail.com>
2023-10-13 17:20:14 +0200

    Bot'Load is done.

--- src/test/java/org/vicary/service/downloader/DownloaderManagerTest.java
+++ src/test/java/org/vicary/service/downloader/DownloaderManagerTest.java
@@ -1,10 +1,12 @@
-package org.vicary.service;
+package org.vicary.service.downloader;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.vicary.format.MarkdownV2;
+import org.vicary.service.Converter;
 import org.vicary.service.downloader.DownloaderManager;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -154,7 +156,7 @@ class DownloaderManagerTest {
     @Test
     void getFileSizeInProcess_expectEquals_ValidLine() {
         //given
-        Long givenBytes = 100000L;
+        long givenBytes = 100000L;
         String givenLine = String.format("[download] File is larger than max-filesize(%d bytes....", givenBytes);
 
         String expectedFileSize = "10.00MB";
@@ -184,7 +186,7 @@ class DownloaderManagerTest {
     @Test
     void getFileSizeInProcess_expectEquals_NegativeBytes() {
         //given
-        Long givenBytes = -100000L;
+        long givenBytes = -100000L;
         String givenLine = String.format("[download] File is larger than max-filesize(%d bytes....", givenBytes);
 
         String expectedFileSize = "-10.00MB";
@@ -364,7 +366,7 @@ class DownloaderManagerTest {
 
 
     @Test
-    void getDownloadFileProgressInProcessInMarkdownV2_expectEquals_ValidLine(){
+    void getDownloadFileProgressInProcess_expectEquals_ValidLine(){
         //given
         String givenLine = "[download] Downloading file: 50.35% of -20.55MiB";
 
@@ -379,7 +381,7 @@ class DownloaderManagerTest {
 
 
     @Test
-    void getDownloadFileProgressInProcessInMarkdownV2_expectEquals_InvalidLine(){
+    void getDownloadFileProgressInProcess_expectEquals_InvalidLine(){
         //given
         String givenLine = "[download] Downloading file: 50?35Percent of -20.55MiB";
 
@@ -394,7 +396,7 @@ class DownloaderManagerTest {
 
 
     @Test
-    void getDownloadFileProgressInProcessInMarkdownV2_expectEquals_WeirdPercents(){
+    void getDownloadFileProgressInProcess_expectEquals_WeirdPercents(){
         //given
         String givenLine = "[download] Downloading file: 50?lol35% of -20.55MiB";
 
@@ -409,7 +411,7 @@ class DownloaderManagerTest {
 
 
     @Test
-    void getDownloadFileProgressInProcessInMarkdownV2_expectEquals_EmptyLine(){
+    void getDownloadFileProgressInProcess_expectEquals_EmptyLine(){
         //given
         String givenLine = "";
 
@@ -420,6 +422,148 @@ class DownloaderManagerTest {
 
         //then
         assertEquals(expectedFileProgress, actualFileProgress);
+    }
+
+
+    @Test
+    void isFileDownloadingInProcess_expectTrue_ValidLine() {
+        //given
+        String givenLine = "[download] Downloading file...";
+
+        //when
+        //then
+        assertTrue(downloaderManager.isFileDownloadingInProcess(givenLine));
+    }
+
+
+    @Test
+    void isFileDownloadingInProcess_expectFalse_InvalidLine() {
+        //given
+        String givenLine = "hello";
+
+        //when
+        //then
+        assertFalse(downloaderManager.isFileDownloadingInProcess(givenLine));
+    }
+
+    @Test
+    void isFileDownloadingInProcess_expectThrow_NullLine() {
+        //given
+        String givenLine = null;
+
+        //when
+        //then
+        assertThrows(NullPointerException.class, () -> downloaderManager.isFileDownloadingInProcess(givenLine));
+    }
+
+
+    @Test
+    void isFileDownloadedInProcess_expectTrue_ValidLine() {
+        //given
+        String givenLine = "[download] File downloading 42MB of 42MB - 100%";
+
+        //when
+        //then
+        assertTrue(downloaderManager.isFileDownloadedInProcess(givenLine));
+    }
+
+
+    @Test
+    void isFileDownloadedInProcess_expectFalse_ValidLine() {
+        //given
+        String givenLine = "[download] File downloading 34MB of 42MB - 85%";
+
+        //when
+        //then
+        assertFalse(downloaderManager.isFileDownloadedInProcess(givenLine));
+    }
+
+
+    @Test
+    void isFileDownloadedInProcess_expectThrow_NullLine() {
+        //given
+        String givenLine = null;
+
+        //when
+        //then
+        assertThrows(NullPointerException.class, () -> downloaderManager.isFileDownloadedInProcess(givenLine));
+    }
+
+
+    @Test
+    void isFileConvertingInProcess_expectTrue_ValidLine() {
+        //given
+        String givenLine = "[ExtractAudio] Converting file...";
+
+        //when
+        //then
+        assertTrue(downloaderManager.isFileConvertingInProcess(givenLine));
+    }
+
+
+    @Test
+    void isFileConvertingInProcess_expectFalse_InvalidLine() {
+        //given
+        String givenLine = "hello";
+
+        //when
+        //then
+        assertFalse(downloaderManager.isFileConvertingInProcess(givenLine));
+    }
+
+    @Test
+    void isFileConvertingInProcess_expectThrow_NullLine() {
+        //given
+        String givenLine = null;
+
+        //when
+        //then
+        assertThrows(NullPointerException.class, () -> downloaderManager.isFileConvertingInProcess(givenLine));
+    }
+
+    @Test
+    void isProgressDifferenceProper_expectTrue_ValidParams() {
+        //given
+        String givenEditTextMessageInMarkdownV2 = "[download] Downloading file: " + MarkdownV2.apply("53.3%").toItalic().get();
+        String givenNewProgress = "65.1%";
+
+        //when
+        //then
+        assertTrue(downloaderManager.isProgressDifferenceProper(givenEditTextMessageInMarkdownV2, givenNewProgress));
+    }
+
+    @Test
+    void isProgressDifferenceProper_expectFalse_TooLowDifference() {
+        //given
+        String givenEditTextMessageInMarkdownV2 = "[download] Downloading file: " + MarkdownV2.apply("53.3%").toItalic().get();
+        String givenNewProgress = "54.2%";
+
+        //when
+        //then
+        assertFalse(downloaderManager.isProgressDifferenceProper(givenEditTextMessageInMarkdownV2, givenNewProgress));
+    }
+
+    @Test
+    void isProgressDifferenceProper_expectFalse_NoNumberInParams() {
+        //given
+        String givenEditTextMessageInMarkdownV2 = "[download] Downloading file: " + MarkdownV2.apply("53.3%").toItalic().get();
+        String givenNewProgress = "hello%";
+
+        //when
+        //then
+        assertFalse(downloaderManager.isProgressDifferenceProper(givenEditTextMessageInMarkdownV2, givenNewProgress));
+    }
+
+
+    @Test
+    void isProgressDifferenceProper_expectFalse_ProgressAlready100Percent() {
+        //given
+        String givenEditTextMessageInMarkdownV2 = "[download] Downloading file: " + MarkdownV2.apply("100%").toItalic().get();
+        String givenNewProgress = "hello%";
+
+        //when
+        //then
+        assertFalse(downloaderManager.isProgressDifferenceProper(givenEditTextMessageInMarkdownV2, givenNewProgress));
     }
 }
 
